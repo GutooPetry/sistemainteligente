3cb591ec282989ac4fbf9d4325cd3a0d3230c633
GutooPetry <gustavoprogrammer24@gmail.com>
2024-06-24 10:39:47 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -697,7 +697,7 @@ def secao_vendas():
 
 def main():
     st.set_page_config(page_title="Estoque Inteligente", layout="wide", page_icon="🍻")
-    # css_botao()
+    css_botao()
     if 'authenticated' not in st.session_state:
         st.session_state['authenticated'] = False
 
@@ -709,7 +709,139 @@ def main():
         login()
 
 
+def css_botao():
+    # Adiciona o CSS personalizado
+    css = """
+    <style>
+    .stButton>button {
+        background-color: #041360;
+        color: #dae6e6;
+        text-align: center;
+        display: inline-block;
+        width: 100%;
+        padding: 12px;
+    }
+    .stButton>button:hover {
+        background-color: #271bda;
+        color: #dae6e6;
+    }
+    .e1f1d6gn4:nth-child(7) .ef3psqc12:hover {
+        background-color: #942020;
+        color: white;
+    }
+    .e1f1d6gn4:nth-child(7) .ef3psqc12 {
+        color: #dae6e6;
+        background-color: #6d0f0f;
+    }
+    .e1f1d6gn4:nth-child(9) .ef3psqc12{
+        background-color: #041360;
+        color: #dae6e6;
+    }
+    .e1f1d6gn4:nth-child(9) .ef3psqc12:hover {
+        background-color: #271bda;
+        color: #dae6e6;
+    }
+    .e1f1d6gn4:nth-child(8) .ef3psqc12{
+        color: #dae6e6;
+        background-color: #6d0f0f;
+        width: 100%;
+        padding: 0px;
+    }
+    .e1f1d6gn4:nth-child(8) .ef3psqc12:hover {
+        background-color: #942020;
+        color: white;
+    }
+    .e1f1d6gn4:nth-child(5) .ef3psqc7 {
+        color: #dae6e6;
+        background-color: #6d0f0f;
+    }
+    .e1f1d6gn4:nth-child(5) .ef3psqc7:hover {
+        background-color: #942020;
+        color: white;
+    }
+    .e1f1d6gn4:nth-child(6) .ef3psqc12 {
+        color: #dae6e6;
+        background-color: #096004;
+    }
+    .e1f1d6gn4:nth-child(6) .ef3psqc7 {
+        color: #dae6e6;
+        background-color: #6d0f0f;
+    }
+    .e1f1d6gn4:nth-child(6) .ef3psqc7:hover {
+        background-color: #942020;
+        color: white;
+    }
+    .e1f1d6gn4:nth-child(6) .ef3psqc12:hover {
+        color: #dae6e6;
+        background-color: #12b716;
+    }
+    .e1f1d6gn4:nth-child(3) .ef3psqc7 {
+        color: #dae6e6;
+        background-color: #096004;
 
+    }
+    .e1f1d6gn4:nth-child(3) .ef3psqc7:hover {
+        color: #dae6e6;
+        background-color: #12b716;
+    }
+    .e10yg2by1+ .e10yg2by1 .ef3psqc7 {
+        color: #dae6e6;
+        background-color: #6d0f0f;
+        padding: 0px
+    }
+    .e10yg2by1+ .e10yg2by1 .ef3psqc7:hover {
+        background-color: #942020;
+        color: white;
+    }
+    .ea3mdgi8 {
+        background-color: #0c1020;
+    }
+    .ezrtsby2 {
+        background-color: #0c1020;
+    }
+    .eczjsme3 {
+        background-color: #1a1a1d;
+    }
+    .e1nzilvr1 {
+        color: #bec1ca;
+    }
+    #root :nth-child(1) {
+        color: #dae6e6;
+    }
+    .st-emotion-cache-uko8fv .ef3psqc7 {
+        background-color: #041360;
+        color: #dae6e6;
+    }
+    .st-bx, .st-b9, .st-bw, .e116k4er1, .st-bb, .st-by, .e116k4er3 {
+        background-color: #1a1a1d;
+        border-color: #041360;
+    }
+    .st-di {
+        background-color: #0c1020;
+        border-color: #041360;
+    }
+    .eczjsme8 {
+        background-color: #0c0c33;
+    }
+    #number_input_4, #number_input_5 {
+        background-color: #1a1a1d;
+        border-color: #041360;
+    }
+    .e16zdaao0 {
+        color: #dae6e6;
+        background-color: #096004;
+        width: 100%;
+        padding: 12px;
+    }
+    .e16zdaao0: hover {
+        color: #dae6e6;
+        background-color: #12b716;
+        width: 100%;
+        padding: 12px;
+    }
+    </style>
+    """
+    return st.markdown(css, unsafe_allow_html=True)
 
 
 # Chamada da função de login
